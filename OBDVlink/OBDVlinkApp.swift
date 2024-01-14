c79d351981666e460d10a4506721a154a3a945c8
Ivan Dimitrov <biobeehive@abv.bg>
2024-01-14 06:23:29 +0200

    Initial Commit

---
+++ OBDVlink/OBDVlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBDVlinkApp.swift
+//  OBDVlink
+//
+//  Created by Dosi Dimitrov on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct OBDVlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
